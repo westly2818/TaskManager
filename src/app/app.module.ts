7a88f289745c79dd00d0f4b08c9c0cfc33d33278
westly <westlydavid.f@blp.co.in>
2023-07-03 14:17:42 +0530

    dashboard done'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
 @NgModule({
   declarations: [
     AppComponent,
@@ -37,7 +38,8 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
     MatDatepickerModule,
     MatNativeDateModule,
     NgxDaterangepickerMd.forRoot(),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatTooltipModule
 
     // DropdownModule
     //  NgbDatepickerModule, NgbModal
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormArray, Validators, FormGroup, FormControl } from "@angular/forms";
 import { ModalDismissReasons, NgbDatepickerModule, NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FirebaseService } from 'src/services/firebase.service'
 import * as moment from 'moment';
 import { log } from 'console';
-
+import * as _ from "lodash"
+import { MatTooltipModule } from '@angular/material/tooltip';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -27,7 +28,7 @@ export class DashboardComponent implements OnInit {
   leads: any = ['Sam', 'Venkatesh']
   filterdData: any = []
   myForm: any = []
-
+  tableData: any = []
   obj: any = {
     'name': '',
     'team': '',
@@ -43,46 +44,101 @@ export class DashboardComponent implements OnInit {
     'timeline': '',
     'endTime': ''
   }
+
+  maxDate: any = moment()
   taskFormObj: any = { 'product': '', 'projectCode': '', 'task_type': '', 'assignedBy': '', 'timeline': '' }
   selected: any
   HistorycollectionName: any = ''
   LiveCollectionName: any = ''
-  constructor(private FirebaseService: FirebaseService, private modalService: NgbModal, public fb: FormBuilder,) { }
+  disableAdd: any = false
+  disableUpdate: any = false
+  cardData: any = [{ title: 'Total Task', value: 0 }, { title: 'Total Completed', value: 0 }, { title: 'Task OnHold', value: 0 }, { title: ' Pending Task', value: 0 },]
+  constructor(private FirebaseService: FirebaseService, private modalService: NgbModal, public fb: FormBuilder,) {
+    // this.minDate.setDate(this.minDate.getDate() - 0);
+  }
 
   ngOnInit(): void {
     let teamID = this.employeeteam.replace(/ /gi, '_').toLowerCase()
-    console.log(teamID);
 
     this.HistorycollectionName = `/TaskData/${teamID}/${this.empCode}/history_data/datalake`
     this.LiveCollectionName = `/TaskData/${teamID}/${this.empCode}/live_data/datalake`
     this.myForm = new FormGroup({
-      product: new FormControl(''),
-      code: new FormControl(''),
-      type: new FormControl(''),
-      assignedBy: new FormControl(''),
-      timeline: new FormControl(''),
-      description: new FormControl('')
+      product: new FormControl('', [Validators.required]),
+      code: new FormControl('', [Validators.required]),
+      type: new FormControl('', [Validators.required]),
+      assignedBy: new FormControl('', [Validators.required]),
+      timeline: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required])
 
     });
-
+    this.getMyTasks()
 
   }
 
-  getdoc() {
+  getMyTasks() {
+    this.tableData = []
+    let liveParam = {
+      category: '',
+      collectionName: this.LiveCollectionName,
+      conditions: []
+    }
+    this.FirebaseService.getData(liveParam).subscribe((res: any) => {
+      res.forEach((data: any) => {
+        let myObj = data.data()
+        if (myObj.status == 'In Progress') {
+          this.disableAdd = true
+          this.disableUpdate = false
+        }
+        else {
+          this.disableAdd = false
+          this.disableUpdate = true
+
+        }
+      })
+    })
     let params = {
       category: '',
-      collectionName: 'checkdata',
+      collectionName: this.HistorycollectionName,
       conditions: []
     }
     this.FirebaseService.getData(params).subscribe((res: any) => {
-      console.log(res);
+      res.forEach((data: any) => {
+        let myObj = data.data()
+
+
+        myObj.startTime = moment(myObj.startTime.seconds * 1000).format('DD-MM-YYYY HH:mm')
+        myObj.endTime = moment(myObj.endTime.seconds * 1000).format('DD-MM-YYYY HH:mm')
+
+        myObj.timeline = moment(myObj.timeline.seconds * 1000).format('DD-MM-YYYY HH:mm')
+
+        this.tableData.push(myObj)
+      })
+      this.tableData = _.orderBy(this.tableData, ['startTime'], ['desc'])
+      let totalTask = this.tableData.filter((ele: any) => {
+        return ele.status == 'In Progress'
+      })
+
+      let completed = this.tableData.filter((ele: any) => {
+        return ele.status == 'Completed'
+      })
+      let onhold = this.tableData.filter((ele: any) => {
+        return ele.status == 'On Hold'
+      })
+      let pending = this.tableData.filter((ele: any) => {
+        return ele.status == 'Pending'
+      })
+      this.cardData[0].value = totalTask.length
+      this.cardData[1].value = completed.length
+
+      this.cardData[2].value = onhold.length
+
+      this.cardData[3].value = pending.length
 
     })
   }
   assignData(data: any) {
+    this.modalService.dismissAll()
     let date = moment(data.value.timeline.startDate.$d).subtract(330, 'minute').toDate()
-
-
     this.obj.branch = this.branch
     this.obj.team = this.employeeteam
     this.obj.name = this.nameOFemployee
@@ -111,45 +167,45 @@ export class DashboardComponent implements OnInit {
     this.FirebaseService.insertData(updateLive).then((res: any) => {
       console.log('done updated live');
 
+      this.getMyTasks()
+
     })
 
-    console.log(this.obj);
 
   }
-  updateStatus(status: any) {
+  async updateStatus(status: any) {
+
     let params = {
       category: '',
-      collectionName: this.LiveCollectionName,
-      conditions: []
+      collectionName: this.HistorycollectionName,
+      conditions: [{ key: 'status', symbol: '==', value: 'In Progress' }]
     }
-    this.FirebaseService.getData(params).subscribe((res: any) => {
+    await this.FirebaseService.getData(params).subscribe((res: any) => {
       res.forEach((ele: any) => {
         let data = ele.data()
         console.log(data);
-        if (data.status != 'In Progress') {
-
+        let id = ele.id
+        data.status = status
+        data.endTime = moment().toDate()
+        let insertParam = {
+          collectionName: this.HistorycollectionName,
+          doc: id,
+          data: data
         }
-        else {
-          data.status = status
-          data.endTime = moment().toDate()
-          let insertParam = {
-            collectionName: this.HistorycollectionName,
-            data: data
-          }
-          let updateLive = {
-            collectionName: this.LiveCollectionName,
-            doc: this.nameOFemployee,
-            data: data
-          }
-          this.FirebaseService.insertData(insertParam).then((res: any) => {
-            console.log('done insertion history');
-
-          })
-          this.FirebaseService.insertData(updateLive).then((res: any) => {
-            console.log('done updated live');
-
-          })
+        let updateLive = {
+          collectionName: this.LiveCollectionName,
+          doc: this.nameOFemployee,
+          data: data
         }
+        this.FirebaseService.insertData(insertParam).then((res: any) => {
+          console.log('done insertion history');
+
+        })
+        this.FirebaseService.insertData(updateLive).then((res: any) => {
+          console.log('done updated live');
+
+          this.getMyTasks()
+        })
       })
 
 
