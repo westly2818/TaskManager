641ea57011e81ea19ab0162a3149ac392c230392
westly <westlydavid.f@blp.co.in>
2023-07-23 13:45:34 +0530

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { FirebaseService } from 'src/services/firebase.service'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,12 +9,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   activeTab: any = 'login'
-  constructor() { }
+  org: any = ['BLP']
+  teams: any = ['Product Development', 'Edge', 'QA', 'Visual Analytics', 'IOT', 'BD', 'Data Analytics']
+  newuserForm: any = []
+  loginform: any = []
+  constructor(private FirebaseService: FirebaseService, private router: Router) { }
 
   ngOnInit(): void {
+    this.newuserForm = new FormGroup({
+      team: new FormControl('', [Validators.required]),
+      organization: new FormControl('', [Validators.required]),
+      mailid: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmpassword: new FormControl('', [Validators.required]),
+    });
+    this.loginform = new FormGroup({
+      id: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    })
   }
   changeTab(tab: any) {
     this.activeTab = tab
+  }
+  passwordMatchValidator(control: any) {
+    const password = control.get('password').value;
+    const confirmPassword = control.get('confirmpassword').value;
+
+    return password === confirmPassword ? null : { 'passwordMismatch': true };
+  }
+  saveuser(data: any) {
+    console.log(data);
+
+    let userobj = {
+      team: data.value.team,
+      name: data.value.name,
+      mailid: data.value.mailid,
+      password: data.value.password,
+      organization: data.value.organization,
+      role: 'Employee'
+    }
+    let userparam = {
+      category: '',
+      collectionName: 'users',
+      conditions: [{ key: 'mailid', symbol: '==', value: userobj.mailid }]
+    }
+    this.FirebaseService.getData(userparam).subscribe((res: any) => {
+      if (res.size > 0) {
+
+      }
+      else {
+        let inserUser = {
+          collectionName: 'users',
+          data: userobj,
+          conditions: []
+
+        }
+        this.FirebaseService.insertData(inserUser).then((res: any) => {
+          console.log('done insertion users');
+
+        })
+      }
+    })
+  }
+  checkuser(form: any) {
+    let userobj = { mailid: form.value.id, pass: form.value.password }
+    let userparam = {
+      category: '',
+      collectionName: 'users',
+      conditions: [{ key: 'mailid', symbol: '==', value: userobj.mailid }]
+    }
+    this.FirebaseService.getData(userparam).subscribe((res: any) => {
+      if (res.size > 0) {
+        res.forEach((element: any) => {
+          let obj = element.data()
+          if (obj.password == userobj.pass) {
+            console.log('user there', obj);
+            this.router.navigate(['/dashboard'])
+          }
+
+
+        });
+
+      }
+      else {
+
+      }
+    })
   }
 
 }
