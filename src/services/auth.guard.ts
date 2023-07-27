cfc4a8b6b43e276f34816d9a9b2351978edeac0c
westly <westlydavid.f@blp.co.in>
2023-07-27 10:09:45 +0530

    fixed

---
+++ src/services/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Router, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    // const isAuth = this.authService.getIsAuth();
+    let isAuth = localStorage.getItem("email") ? true : false;
+
+    if (!isAuth) {
+      this.router.navigate(['/login']);
+    }
+    return isAuth;
+  }
+}
