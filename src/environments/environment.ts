b28d07614c85151e3f52d91ed2988a5a0027235a
westly2818 <125299635+westly2818@users.noreply.github.com>
2023-08-26 17:30:39 +0530

    Update environment.ts
    removed firebase key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,6 @@ export const environment = {
   production: false,
   apiUrl: 'https://api.example.com',
   firebase: {
-    apiKey: "AIzaSyCN4tfwZPJBDnz88fNLyyxDUsgWU9p9pSs",
-    authDomain: "taskmanager-5984b.firebaseapp.com",
-    projectId: "taskmanager-5984b",
-    storageBucket: "taskmanager-5984b.appspot.com",
-    messagingSenderId: "176330936303",
-    appId: "1:176330936303:web:fad99dd34838c0b447701a"
+   //your firebase key
   }
 };
